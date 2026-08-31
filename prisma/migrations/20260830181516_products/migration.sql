bcc41faf1d49d092df91c1186c43a21a0b831ddc
Sodiq Eniola <sodeeqe03@gmail.com>
2026-08-31 06:51:21 +0100

    feat: add migration script to drop unused indexes

---
+++ prisma/migrations/20260830181516_products/migration.sql
@@ -0,0 +1,32 @@
+-- DropIndex
+DROP INDEX "community_offers_title_trgm_idx";
+
+-- DropIndex
+DROP INDEX "community_requests_description_trgm_idx";
+
+-- DropIndex
+DROP INDEX "community_requests_title_trgm_idx";
+
+-- DropIndex
+DROP INDEX "groups_name_trgm_idx";
+
+-- DropIndex
+DROP INDEX "guides_intro_trgm_idx";
+
+-- DropIndex
+DROP INDEX "guides_title_trgm_idx";
+
+-- DropIndex
+DROP INDEX "professional_listings_title_trgm_idx";
+
+-- DropIndex
+DROP INDEX "request_responses_one_offer_per_author_idx";
+
+-- DropIndex
+DROP INDEX "reviews_one_prior_work_per_pair_idx";
+
+-- DropIndex
+DROP INDEX "store_items_name_trgm_idx";
+
+-- DropIndex
+DROP INDEX "stores_name_trgm_idx";
